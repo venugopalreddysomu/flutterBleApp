04235dfb0c0bc5d00a3870cd7cac85115016d779
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-27 02:23:42 +0000

    disabled back navigtionfrom Home pge to splashscreen

--- lib/pages/splash_page/splash_page_widget.dart
+++ lib/pages/splash_page/splash_page_widget.dart
@@ -33,10 +33,8 @@ class _SplashPageWidgetState extends State<SplashPageWidget> {
     SchedulerBinding.instance.addPostFrameCallback((_) async {
       await requestPermission(bluetoothPermission);
       _model.bluetoothEnabled = await actions.isBluetoothEnabled();
-      if (Navigator.of(context).canPop()) {
-        context.pop();
-      }
-      context.pushNamed(
+
+      context.goNamed(
         'HomePage',
         queryParameters: {
           'isBTEnabled': serializeParam(
