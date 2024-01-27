71e12d04ca820e874ae9045a1cb0d017b44d47ab
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-27 02:37:53 +0000

    Updated Bluetooth Enable custom function

--- lib/custom_code/actions/turn_on_bluetooth.dart
+++ lib/custom_code/actions/turn_on_bluetooth.dart
@@ -12,7 +12,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 Future<bool> turnOnBluetooth() async {
   if (isAndroid) {
-    return await FlutterBluePlus.instance.turnOn();
+    return await FlutterBluePlus.turnOn();
   }
   return true;
 }
