ec404ab0b4357845851bf9a9d16e94ac64c6db07
venugopalreddysomu <venugopal@bridgethings.com>
2024-02-01 05:53:03 +0530

    Update reverted

--- lib/custom_code/actions/turn_on_bluetooth.dart
+++ lib/custom_code/actions/turn_on_bluetooth.dart
@@ -13,7 +13,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 Future<bool> turnOnBluetooth() async {
   if (isAndroid) {
-    return await FlutterBluePlus.turnOn();
+    return await FlutterBluePlus.instance.turnOn();
   }
   return true;
 }
